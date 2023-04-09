a48c95ef6c9322fcf5490d09d417f63b73b5e139
Le Tien <ww.hacker01@email.com>
2023-04-09 22:47:23 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace GameBanGa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.tm_Bullets = new System.Windows.Forms.Timer(this.components);
+            this.tm_Chickens = new System.Windows.Forms.Timer(this.components);
+            this.tm_Eggs = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(56, 16);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // tm_Bullets
+            // 
+            this.tm_Bullets.Tick += new System.EventHandler(this.tm_Bullets_Tick);
+            // 
+            // tm_Chickens
+            // 
+            this.tm_Chickens.Tick += new System.EventHandler(this.tm_Chickens_Tick);
+            // 
+            // tm_Eggs
+            // 
+            this.tm_Eggs.Tick += new System.EventHandler(this.tm_Eggs_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer tm_Bullets;
+        private System.Windows.Forms.Timer tm_Chickens;
+        private System.Windows.Forms.Timer tm_Eggs;
+    }
+}
+
