0519015def8947e714a472ce62bd9fe6d0113402
Le Tien <ww.hacker01@email.com>
2023-04-09 23:09:07 +0700

    Update 1

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,22 @@ namespace GameBanGa
 {
     public partial class Form1 : Form
     {
+        private Piece rocket;
+
         public Form1()
         {
             InitializeComponent();
+            initial();
+        }
+
+        private void initial()
+        {
+            rocket = new Piece(100, 100);
+            rocket.Left = this.Width / 2 - rocket.Width / 2;
+            rocket.Top = this.Height - rocket.Height;
+            rocket.Image = Properties.Resources.ship;
+
+            this.Controls.Add(rocket);
         }
 
         private void tm_Bullets_Tick(object sender, EventArgs e)
---
+++ PIece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameBanGa
+{
+    public class Piece : PictureBox
+    {
+        public Piece(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            SizeMode = PictureBoxSizeMode.StretchImage;
+            BackColor = Color.Transparent;
+        }
+    }
+}
